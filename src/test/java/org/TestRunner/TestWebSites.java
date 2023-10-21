68b34d5323630c838fb6c1fc20129955edb1cc61
Sayan <sayanbhandari007@gmail.com>
2023-10-21 16:50:00 +0530

    updates in progress till time 4.45

--- src/test/java/org/TestRunner/TestWebSites.java
+++ src/test/java/org/TestRunner/TestWebSites.java
@@ -1,7 +1,11 @@
 package org.TestRunner;
 
 import org.browserutilities.decideBrowser;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class TestWebSites extends decideBrowser {
 
@@ -14,31 +18,32 @@ public class TestWebSites extends decideBrowser {
     public void openBrowser() {
         driver = decideBrowser.passBrowserData("chrome");
         assert driver != null;
-
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
 
-    @Test(priority = 1, groups = { "regressiontest", "smoketest" })
+    @Test(priority = 1, groups = {"RegressionTest", "SmokeTest"})
     public void indeedWebsiteTest() {
         driver.get(indeedUrl);
         System.out.println("indeedWebsite");
 
     }
 
-    @Test(priority = 2, groups = { "regressiontest", "smoketest" })
-    public void baytWebsiteTest(){
+    @Test(priority = 2, groups = {"RegressionTest", "SmokeTest"})
+    public void baytWebsiteTest() {
         driver.get(baytUrl);
         System.out.println("baytWebsite");
     }
 
 
-    @Test(priority = 3, groups = { "regressiontest" })
-    public void gulftalentWebsiteTest(){
+    @Test(priority = 3, groups = {"RegressionTest", "SmokeTest"})
+    public void gulfTalentWebsiteTest() {
         driver.get(gulfTalentUrl);
         System.out.println("gulfTalent");
     }
 
     @AfterMethod()
-    public void closeBrowser(){
+    public void closeBrowser() {
         driver.quit();
     }
 
